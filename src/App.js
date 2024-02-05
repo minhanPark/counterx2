6fd3f708f275abfc37b2a01dbbc1e4f8e1a8f10b
minhanPark <alsgks1218@naver.com>
2024-02-05 11:12:11 +0900

    App.js에서 counter/index.jsx로 state 옮기고 불필요한 부분들 삭제

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { useState } from "react";
-
 import "./App.css";
-import CounterText from "./components/counter-text";
+
 import Counter from "./components/counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-  const hanldeDecrement = () => {
-    setCount(count - 1);
-  };
   return (
     <div className="wrapper">
       <h1 className="header">카운터 x 2</h1>
--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,12 +1,22 @@
 import CounterText from "../counter-text";
 import "./index.css";
 
+import { useState } from "react";
+
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const hanldeDecrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="counter-wrapper">
-      <button onClick={() => {}}>-1</button>
+      <button onClick={hanldeDecrement}>-1</button>
       <CounterText />
-      <button onClick={() => {}}>+1</button>
+      <button onClick={handleIncrement}>+1</button>
     </div>
   );
 }

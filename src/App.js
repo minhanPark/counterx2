a9320f1c6788647e78ce1d2724598af38f72e875
minhanPark <alsgks1218@naver.com>
2024-02-05 10:56:07 +0900

    counter ui 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="wrapper">
       <h1 className="header">카운터 x 2</h1>
+      <div className="counter-wrapper">
+        <button>-1</button>
+        <p className="counter-text">0</p>
+        <button>+1</button>
+      </div>
     </div>
   );
 }

5b4b773155b80c016b4cf41a4f0fcf4be782e5b9
minhanPark <alsgks1218@naver.com>
2024-02-05 10:59:01 +0900

    useState를 추가해서 count 증감 하도록 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const hanldeDecrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="wrapper">
       <h1 className="header">카운터 x 2</h1>
       <div className="counter-wrapper">
-        <button>-1</button>
-        <p className="counter-text">0</p>
-        <button>+1</button>
+        <button onClick={hanldeDecrement}>-1</button>
+        <p className="counter-text">{count}</p>
+        <button onClick={handleIncrement}>+1</button>
       </div>
     </div>
   );

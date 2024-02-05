bdf3cfb8b0f4278c1b4d4ecdef7ef0774607b17b
minhanPark <alsgks1218@naver.com>
2024-02-05 10:41:04 +0900

    헤더 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return null;
+  return (
+    <div className="wrapper">
+      <h1 className="header">카운터 x 2</h1>
+    </div>
+  );
 }
 
 export default App;
